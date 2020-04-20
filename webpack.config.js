f03b57649a9eacd5fcb29d90f542445ee6df57b1
LazyTechwork <lazytechwork@gmail.com>
2020-04-20 15:41:57 +0300

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,44 @@
 const path = require('path');
+import webpack from "webpack";
+import TerserPlugin from 'terser-webpack-plugin';
 
 module.exports = {
     entry: './src/index.ts',
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    output: {
+                        comments: /(\s@|UserScript==)/i,
+                    },
+                }
+            })
+        ]
+    },
     output: {
         filename: 'vkfix.user.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ]
+        rules: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "ts-loader",
+                options: {
+                    transpileOnly: true
+                }
+            }
+        }]
     },
     resolve: {
         extensions: [".tsx", ".ts", ".js"]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: fs.readFileSync(path.resolve(__dirname, "src/index.ts"), "utf-8").replace(/(==\/UserScript==)[\s\S]+$/, "$1"),
+            entryOnly: true,
+            raw: true
+        })
+    ]
 };
