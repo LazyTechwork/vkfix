f047c653d04a65928d49c4269eb7d41ad4d9cfec
LazyTechwork <lazytechwork@gmail.com>
2020-04-23 23:05:23 +0300

    Added kick feature

--- src/modules/message_actions.ts
+++ src/modules/message_actions.ts
@@ -1,17 +1,35 @@
 import MessageButton from "../classes/MessageButton";
+import Message from "../classes/Message";
+import ApiInteractor from "../classes/ApiInteractor";
 
 const buttons: MessageButton[] = [{
     icon: 'K',
     action: (ev: any) => {
-    /*    https://vk.com/al_im.php
-          act: a_kick_user
-al: 1
-chat: 2000000270
-gid: 0
-hash: 1587667820_e7126e9df341ed5f59
-im_v: 3
-mid: 171882202
-     */
+        const endpoint = 'https://' + location.host + '/al_im.php';
+        const peerId = Message.getPeerIdClick(ev);
+        const memberId = Message.getIdAuthorClick(ev);
+        const rawData = {
+            act: 'a_kick_user',
+            al: 1,
+            chat: peerId,
+            gid: 0,
+            im_v: 3,
+            mid: memberId
+        };
+        console.log(rawData);
+        ApiInteractor.callApiRaw(rawData, endpoint).then(r => {
+            console.log(r)
+        });
+        /*
+            https://vk.com/al_im.php
+            act: a_kick_user
+            al: 1
+            chat: 2000000270
+            gid: 0
+            im_v: 3
+            mid: 171882202
+         */
+
     },
     tooltip: 'Исключить'
 }];
