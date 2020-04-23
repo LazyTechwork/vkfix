795efc401eba04f5b40dc0c6f982b4ef7fbf00b4
LazyTechwork <lazytechwork@gmail.com>
2020-04-23 19:26:27 +0300

    Added comments

--- src/index.ts
+++ src/index.ts
@@ -14,22 +14,14 @@ import hover_kick from "./hover_kick";
 import styles from "./styles";
 import GM_config from "./libs/GM_config.js";
 
-(function (window, undefined) {  // [2] нормализуем window
+(function (window, undefined) { // Используем замыкание для запуска нашего скрипта
     let w = window;
-
-    // В юзерскрипты можно вставлять практически любые javascript-библиотеки.
-    // Код библиотеки копируется прямо в юзерскрипт.
-    // При подключении библиотеки нужно передать w в качестве параметра окна window
-    // Пример: подключение jquery.min.js
-    // (function(a,b){function ci(a) ... a.jQuery=a.$=d})(w);
-
-    // [3] не запускаем скрипт во фреймах
-    // без этого условия скрипт будет запускаться несколько раз на странице с фреймами
 
     if (w.self != w.top) {
         return;
     }
 
+    // Инициализируем новый конфиг
     let cfg = new GM_config({
         'id': 'vkfix',
         'title': 'Настройка VK Fix',
@@ -47,18 +39,20 @@ import GM_config from "./libs/GM_config.js";
     if (/https:\/\/vk.com/.test(w.location.href)) {
         console.log("VK Fix запущен")
 
+        // Добавляем кнопку настроек в верхнее меню
         const settings_link = document.getElementById('top_settings_link')
         const vkfixconflink = document.createElement('a')
         vkfixconflink.innerHTML = "VK Fix"
         vkfixconflink.id = "top_vkfix_settings_link"
         vkfixconflink.className = "top_profile_mrow"
         vkfixconflink.setAttribute("href", "#")
-        settings_link.parentNode.insertBefore(vkfixconflink, settings_link.nextSibling)
+        settings_link.parentNode.insertBefore(vkfixconflink, settings_link.nextSibling) // Вставляем после ссылки на настройки
         vkfixconflink.addEventListener('click', (ev) => {
             ev.preventDefault()
             cfg.open()
         })
 
+        // Инъекция стилей
         styles()
 
         if (cfg.get('hover_kick'))
