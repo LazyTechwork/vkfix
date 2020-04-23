ce39392b86fce85c57677c4ddf6ebb6ba638edf1
LazyTechwork <lazytechwork@gmail.com>
2020-04-23 21:55:00 +0300

    TODO Api interaction

--- src/modules/message_actions.ts
+++ src/modules/message_actions.ts
@@ -2,7 +2,17 @@ import MessageButton from "../classes/MessageButton";
 
 const buttons: MessageButton[] = [{
     icon: 'K',
-    action: () => alert('Test'),
+    action: () => {
+    /*    https://vk.com/al_im.php
+          act: a_kick_user
+al: 1
+chat: 2000000270
+gid: 0
+hash: 1587667820_e7126e9df341ed5f59
+im_v: 3
+mid: 171882202
+     */
+    },
     tooltip: 'Исключить'
 }];
 
