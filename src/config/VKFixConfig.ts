6417e25e9d75ed9e0453a976053bed371edbf2b8
LazyTechwork <lazytechwork@gmail.com>
2020-04-24 16:06:01 +0300

    Added scaffolding

--- src/config/VKFixConfig.ts
+++ src/config/VKFixConfig.ts
@@ -3,5 +3,19 @@ import ConfigField from "./ConfigField";
 export default class VKFixConfig {
     public config_id: string
     public fields: ConfigField[]
+    private config_node: Node | HTMLElement | Element | null = null
 
+    private modalGenerator() {
+        for (const field of this.fields) {
+
+        }
+    }
+
+    open(parent: Node) {
+
+    }
+
+    close() {
+
+    }
 }
