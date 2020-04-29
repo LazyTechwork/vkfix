6173740b5afb108447a70350793718151eb05774
LazyTechwork <lazytechwork@gmail.com>
2020-04-29 22:00:47 +0300

    Added mutation on load

--- src/modules/mutations/message_mutations.ts
+++ src/modules/mutations/message_mutations.ts
@@ -13,16 +13,15 @@ export default function (mutation: MutationRecord) {
     if (!classes) return; // Если их нет - переходим дальше
     if (classes.contains('im-mess')) { // Если цель мутации - сообщение
         let isActMsg = classes.contains("im-mess_srv")
+        console.log(isActMsg)
         for (let n of target.children) { // Получаем всех детей элемента
             if (!n.classList) continue // Если у ребенка нет классов переходим дальше
-            if (isActMsg) {
-                if (n.classList.contains("im-mess--text")) {
-                    action_mutation_act(n)
-                }
-            }
-            if (n.classList.contains('im-mess--actions')) { // Если это группа действий и в ней нет наших кнопок - добавляем их
+
+            if (isActMsg && n.classList.contains("im-mess--text"))
+                action_mutation_act(n)
+
+            if (n.classList.contains('im-mess--actions'))  // Если это группа действий и в ней нет наших кнопок - добавляем их
                 mutation_act(n)
-            }
         }
     }
 }
--- src/modules/mutations/mutation_handler.ts
+++ src/modules/mutations/mutation_handler.ts
@@ -1,9 +1,18 @@
-import message_mutations, {mutation_act} from "./message_mutations";
+import message_mutations, {action_mutation_act, mutation_act} from "./message_mutations";
 
 export default function () {
 // При инциализации скрипта пробегаемся по всем сообщениям и добавляем кнопки
-    for (const n of document.getElementsByClassName('im-mess--actions')) {
-        mutation_act(n)
+    for (const msgNodes of document.getElementsByClassName('im-mess')) {
+        let isActMsg = msgNodes.classList.contains("im-mess_srv")
+        for (let n of msgNodes.children) { // Получаем всех детей элемента
+            if (!n.classList) continue // Если у ребенка нет классов переходим дальше
+
+            if (isActMsg && n.classList.contains("im-mess--text"))
+                action_mutation_act(n)
+
+            if (n.classList.contains('im-mess--actions'))  // Если это группа действий и в ней нет наших кнопок - добавляем их
+                mutation_act(n)
+        }
     }
 
     // Настраиваем слежение мутаций в DOM
