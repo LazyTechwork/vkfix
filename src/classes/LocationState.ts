97ee2b9495d57903d970dbb752f65d038a52748e
LazyTechwork <lazytechwork@gmail.com>
2020-04-30 13:10:01 +0300

    Location change listener

--- src/classes/LocationState.ts
+++ src/classes/LocationState.ts
@@ -1,16 +1,27 @@
 import VKLocation from "./VKLocation";
+import location_mutations from "../modules/mutations/location_mutations";
 
 export default class LocationState {
     private static previousQuery: URLSearchParams | null = null
+    private static previousHref: string | null = null
     private static query: URLSearchParams | null = null
+    private static href: string | null = null
 
-    public static changeQuery(newQuery: URLSearchParams) {
+    private static locUpdScanner: number | null = null
+
+    public static changeState(href: string, newQuery: URLSearchParams) {
         this.previousQuery = this.query
+        this.previousHref = this.href
         this.query = newQuery
+        this.href = href
     }
 
-    public static updateQuery() {
-        this.changeQuery(VKLocation.getQueryParams())
+    public static updateState() {
+        this.changeState(location.href, VKLocation.getQueryParams())
+        console.log("Updated location", {
+            previousQuery: this.previousQuery ? this.previousQuery.get("sel") : null,
+            query: this.query ? this.query.get("sel") : null
+        })
     }
 
     public static getCurrentQuery() {
@@ -19,5 +30,15 @@ export default class LocationState {
 
     public static getPreviousQuery() {
         return this.previousQuery
+    }
+
+    public static locationScanner() {
+        if (this.locUpdScanner !== null) clearInterval(this.locUpdScanner)
+        this.locUpdScanner = setInterval(() => {
+            if (location.href !== this.href) {
+                this.updateState()
+                location_mutations(null)
+            }
+        }, 1000)
     }
 }
