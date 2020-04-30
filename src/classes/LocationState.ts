45ae0c5934aa4d017503e8766598082e618dd50e
LazyTechwork <lazytechwork@gmail.com>
2020-04-30 12:07:08 +0300

    LocationState listeners

--- src/classes/LocationState.ts
+++ src/classes/LocationState.ts
@@ -1,3 +1,5 @@
+import VKLocation from "./VKLocation";
+
 export default class LocationState {
     private static previousQuery: URLSearchParams | null = null
     private static query: URLSearchParams | null = null
@@ -5,6 +7,10 @@ export default class LocationState {
     public static changeQuery(newQuery: URLSearchParams) {
         this.previousQuery = this.query
         this.query = newQuery
+    }
+
+    public static updateQuery() {
+        this.changeQuery(VKLocation.getQueryParams())
     }
 
     public static getCurrentQuery() {
