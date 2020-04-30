7b248a2665c4ea7d704a10226e4f66a6612f6de4
LazyTechwork <lazytechwork@gmail.com>
2020-04-30 12:14:30 +0300

    Exported page scanner out of module

--- src/modules/mutations/location_mutations.ts
+++ src/modules/mutations/location_mutations.ts
@@ -1,10 +1,11 @@
 import LocationState from "../../classes/LocationState";
+import page_scanner from "../page_scanner";
 
 export default function (ev: any) {
     LocationState.updateQuery()
     let cq = LocationState.getCurrentQuery()
     let pq = LocationState.getPreviousQuery()
     if (cq.get("sel") != pq.get("sel")) {
-
+        page_scanner()
     }
 }
