e8d9ab0874b7540c0ee123f2bc7a9aefe6490416
Ivan Melentev <ivan.saxalin@mail.ru>
2020-05-17 15:27:45 +0300

    конфигурация для кнопок ботов

--- src/GlobalConfig.ts
+++ src/GlobalConfig.ts
@@ -1,15 +1,39 @@
 import GM_config from "./libs/GM_config";
 
+export const defaultBotButtons = [{
+    icon: 'И',
+    text: "Исключить",
+    command: 'кик'
+}, {
+    icon: '+',
+    text: "Плюсануть",
+    command: '+'
+}, {
+    icon: 'С',
+    text: "Статистика",
+    command: 'стата'
+}];
+
 export default class GlobalConfig {
     static Config = new GM_config({
         'id': 'vkfix',
         'title': 'Настройка VK Fix',
         'fields': {
             'amadeus': {
-                'label': 'Управление конференциями при помощи Амадеуса',
+                'label': 'Дополнительные кнопки для ботов в беседах',
                 'type': 'checkbox',
                 'default': true,
                 'section': ['Настройки модулей', 'Не забудьте перезагрузить страницу']
+            },
+            'botName': {
+                'label': 'Алиас для обращения к боту',
+                'type': 'text',
+                'default': 'Амадеус',
+            },
+            'botAction': {
+                'label': 'Кнопки для ботов в беседах в виде JSON',
+                'type': 'textarea',
+                'default':  JSON.stringify(defaultBotButtons),
             }
         }
     });
--- src/modules/amadeus_actions.ts
+++ src/modules/amadeus_actions.ts
@@ -1,24 +1,22 @@
 import AmadeusConfig from "../classes/AmadeusConfig";
 import AmadeusButton from "../classes/AmadeusButton";
-import GlobalConfig from "../GlobalConfig";
+import GlobalConfig, {defaultBotButtons} from "../GlobalConfig";
+let aliasBot = GlobalConfig.Config.get('botName') ?? '';
+let botAction: any = [];
+try {
+    botAction = JSON.parse(GlobalConfig.Config.get('botAction')) ?? [];
+}catch {
+    botAction = defaultBotButtons;
+}
+if(aliasBot.length > 0) aliasBot = `${aliasBot} `;
 
 AmadeusConfig.Global = {
-    prevCommandText: 'Амадеус ',
+    prevCommandText: aliasBot,
     replyMessage: true,
-    buttons: [{
-        icon: 'И',
-        text: "Исключить",
-        command: 'кик'
-    }, {
-        icon: '+',
-        text: "Плюсануть",
-        command: '+'
-    }, {
-        icon: 'С',
-        text: "Статистика",
-        command: 'стата'
-    }]
+    buttons: botAction
 };
+
+console.log(botAction);
 
 const isModuleEnabled = GlobalConfig.Config.get('amadeus');
 
