296b70d6adbad4682e702241c722f68bac8eb582
Ivan Melentyev <ivan.saxalin@mail.ru>
2025-05-23 03:16:25 +0300

    release: 1.1.7

--- dist/vkfix.user.js
+++ dist/vkfix.user.js
@@ -4,7 +4,7 @@
 // @author Ivan Petrov (LazyTechwork)
 // @contributors Ivan Mel (xeleoss)
 // @license MIT
-// @version 1.1.6
+// @version 1.1.7
 // @include https://vk.com/*
 // @grant GM_getValue
 // @grant GM_setValue
@@ -14064,7 +14064,7 @@ function createVkUiButton(innerHTML, click) {
 // @author Ivan Petrov (LazyTechwork)
 // @contributors Ivan Mel (xeleoss)
 // @license MIT
-// @version 1.1.6
+// @version 1.1.7
 // @include https://vk.com/*
 // @grant GM_getValue
 // @grant GM_setValue
